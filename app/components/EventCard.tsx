719af8757bf2cd944bfe86af384c02b7e22f88b9
Cali Carpenter <calicarpenter07@gmail.com>
2024-08-02 21:19:49 +0000

    Aug 2, 2024, 5:19 PM

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         padding: 10,
         height: 180
-    
+        width: 
     },
     imageContainer: {
         flex: 1,
@@ -66,10 +66,11 @@ const styles = StyleSheet.create({
     },
     placeholder: {
         width: '100%',
-        height: 150,
+        height: '100%',
         
         justifyContent: 'center',
         alignItems: 'center',
+        borderRadius: 10,
     },
     placeholderText: {
         color: '#555',
