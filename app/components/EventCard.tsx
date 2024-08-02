24f2c0ae70ec9731be22ad93e167cfc90dfb4540
Cali Carpenter <calicarpenter07@gmail.com>
2024-08-02 21:15:06 +0000

    Aug 2, 2024, 5:15 PM

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     imageContainer: {
-        padding: 10, // Add padding around the image or placeholder
+        padding: 10, 
     },
     image: {
         width: '100%',
